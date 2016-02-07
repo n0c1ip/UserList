f7db8f7f8e9c4b62b904ed7a6052afc206534bd9
Вячеслав Махнутин <slavazakamsk@gmail.com>
2016-02-08 00:13:54 +0500

    modified departmentservice, userservice

--- src/main/java/crud/UserService.java
+++ src/main/java/crud/UserService.java
@@ -4,43 +4,51 @@ import objects.Location;
 import objects.User;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class UserService {
 
-        public EntityManager manager = Persistence.createEntityManagerFactory("UserList").createEntityManager();
-
-        public User add(User user){
+    public static User add(User user){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
             manager.getTransaction().begin();
             User userFromDB = manager.merge(user);
             manager.getTransaction().commit();
+            manager.close();
             return userFromDB;
         }
 
-        public void delete(long id){
+        public static void delete(long id){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
             manager.getTransaction().begin();
-            manager.remove(get(id));
+            manager.remove(manager.find(User.class, id));
             manager.getTransaction().commit();
+            manager.close();
         }
 
-        public User get(long id){
-            return manager.find(User.class, id);
+        public static User get(long id){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
+            User foundUser = manager.find(User.class, id);
+            manager.close();
+            return foundUser;
         }
 
-        public void update(User user){
+        public static void update(User user){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
             manager.getTransaction().begin();
             manager.merge(user);
             manager.getTransaction().commit();
+            manager.close();
         }
 
-        public List<User> getAll(){
+        public static List<User> getAll(){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
             TypedQuery<User> namedQuery = manager.createNamedQuery("User.getAll", User.class);
             return namedQuery.getResultList();
         }
 
-        public List<User> getUsersByLocationName(String locationName){
+        public static List<User> getUsersByLocationName(String locationName){
+            EntityManager manager = EntityManagerFactory.createEntityManager();
             TypedQuery<Location> getLocationByName = manager.createNamedQuery("Location.getByName", Location.class);
             getLocationByName.setParameter("name", locationName);
             TypedQuery<User> getUserByLocatonName = manager.createNamedQuery("User.getUsersByLocationName", User.class);
