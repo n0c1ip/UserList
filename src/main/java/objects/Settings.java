21c071557187d48463e8d601fd99b2b7d2960a90
Hofls <hofls@ya.ru>
2016-02-16 00:42:06 +0500

    Implemented Settings + SettingsService

---
+++ src/main/java/objects/Settings.java
@@ -0,0 +1,35 @@
+package objects;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable{
+
+    private String userName;
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private String password;
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String server;
+    private String serverPrefix = "jdbc:mysql://";
+    public String getActualServer() {
+        return server;
+    }
+    public String getServer() {
+        return server.replace(serverPrefix, "");
+    }
+    public void setServer(String server) {
+        this.server = serverPrefix + server;
+    }
+
+}
