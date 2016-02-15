62d0b814e0a34c1ab00e58590c25c64a2d44d472
Hofls <hofls@ya.ru>
2016-02-16 00:42:48 +0500

    Added Settings to EntityManagerFactory

--- src/main/java/crud/EntityManagerFactory.java
+++ src/main/java/crud/EntityManagerFactory.java
@@ -1,11 +1,28 @@
 package crud;
 
+import objects.Settings;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityManagerFactory {
 
-    private static javax.persistence.EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("UserList");
+    private static javax.persistence.EntityManagerFactory entityManagerFactory;
+
+    static {
+        Map<String, String> properties = new HashMap<String, String>();
+        Settings settings = SettingsService.readSettings();
+        if (settings != null) {
+            properties.put("hibernate.connection.username", settings.getUserName());
+            properties.put("hibernate.connection.password", settings.getPassword());
+            properties.put("hibernate.connection.url", settings.getActualServer());
+            properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+
+        }
+        entityManagerFactory = Persistence.createEntityManagerFactory("UserList", properties);
+    }
 
     private EntityManagerFactory() {}
 
