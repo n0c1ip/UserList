dcb40596d16118ec1fa22f169b79368ffff6b481
Hofls <hofls@ya.ru>
2016-02-17 00:39:45 +0500

    Implemented Settings test

---
+++ src/test/java/objects/SettingsTest.java
@@ -0,0 +1,21 @@
+package objects;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Set;
+
+
+public class SettingsTest {
+
+    @Test
+    public void shouldGetServerWithPrefix() {
+        String prefix = "jdbc:mysql://";
+        String server = "localhost:3306/testdb";
+        Settings settings = new Settings();
+        settings.setServer(server);
+        Assert.assertEquals(server, settings.getServer());
+        Assert.assertEquals(prefix + server, settings.getServerWithPrefix());
+    }
+
+}
